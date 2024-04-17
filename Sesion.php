e7123d11a232140cfb4dfc615e5814bc1b0f37b6
Dorian Cinue Arce Vázquez <doriancinue26@gmail.com>
2024-04-16 18:06:31 -0600

    1

--- Sesion.php
+++ Sesion.php
@@ -1,11 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-
+include("conection.php");
 $usuario = $_GET['usuario'];
 $password = $_GET['contrasena'];
 
 $sql = "SELECT * FROM usuarios WHERE usuario='$usuario' AND contrasena='$password'";
-$con = mysqli_connect('localhost', 'root', '', 'juegoa');
 $res = mysqli_query($con, $sql);
 $response = array();
 if (mysqli_num_rows($res) > 0) {
